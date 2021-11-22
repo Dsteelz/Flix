0ff98c50fc192f7d926509f6a40531f4a7e42cda
Dsteelz <91301867+Dsteelz@users.noreply.github.com>
2021-11-21 16:20:21 -0800

    Add files via upload

---
+++ Flix/MoiveCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MoiveCellTableViewCell.swift
+//  Flix
+//
+//  Created by Jairef Fowler on 11/20/21.
+//
+
+import UIKit
+
+class MoiveCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
